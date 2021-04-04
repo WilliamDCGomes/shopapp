a7dcc49999f932acd16b7b5c8e62328d5cd1369d
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-04 19:54:18 -0300

    Começando a implementar a viewmodel das lojas

---
+++ ShopApp/ShopApp/ViewModels/StoresViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using ShopApp.Libraries.Enums;
+using ShopApp.Models;
+using ShopApp.Services;
+using Xamarin.Forms;
+
+namespace ShopApp.ViewModels
+{
+    public class StoresViewModel
+    {
+        public string SearchWord { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public List<Establishment> Establishments { get; set; }
+
+        public StoresViewModel()
+        {
+            SearchCommand = new Command(Search);
+            var allEstablishment = new EstablishmentService().GetEstablishment();
+            var allStores = allEstablishment.Where(a=>a.Type == EstablishmentType.Store).ToList();
+            Establishments = allStores;
+        }
+
+        private void Search()
+        {
+
+        }
+    }
+}
